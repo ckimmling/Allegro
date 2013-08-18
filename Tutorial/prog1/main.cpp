4324881045872c7a31045cd14d3b04eb5925a546
ckimmling <ckimmling@users.noreply.github.com>
2013-08-18 15:37:05 +0200

    add movement, collision control and double buffering

--- Tutorial/prog1/main.cpp
+++ Tutorial/prog1/main.cpp
@@ -13,6 +13,10 @@ int set_graphic_mode(int depth)
 
 int main(int argc, char **argv)
 {
+    int x = 0;
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
     // initialize allegro
     allegro_init();
     
@@ -39,12 +43,52 @@ int main(int argc, char **argv)
     clear(screen);
     blit(logo, screen, 
          0 , 0,
-         (SCREEN_W - logo->w)/2,
-         (SCREEN_H - logo->h)/2,
+         x,
+         y,
          logo->w, logo->h);
     
     // release screen and wait for key event
     release_screen();
-    while(!keypressed());
+    
+    BITMAP* doubleBuffer = create_bitmap(SCREEN_W, SCREEN_H);
+    while(!keypressed())
+    {
+        // check if position x reach left border
+        if (x + dx < 0)
+        {
+            // change direction of logo
+            dx *= -1;
+        }
+        // check if position x reach rigth border
+        else if (x + logo->w >= SCREEN_W)
+        {
+            // change direction of logo
+            dx *= -1;
+        }
+        
+        // check if position y reach top border
+        if (y + dy < 0)
+        {
+            dy *= -1;
+        }
+        // check if position y reach bottom border
+        else if (y + logo->h >= SCREEN_H)
+        {
+            dy *= -1;
+        }
+        x+=dx;
+        y+=dy;
+        
+        // first load logo into a second buffer
+        acquire_bitmap(doubleBuffer);
+        clear(doubleBuffer);
+        blit(logo, doubleBuffer, 0 , 0, x, y, logo->w, logo->h);
+        release_bitmap(doubleBuffer);
+        
+        // draw logo on next calculated position
+        acquire_screen();
+        blit(doubleBuffer, screen, 0 , 0, 0, 0, SCREEN_W, SCREEN_H);
+        release_screen();
+    }
     return 0;
 } END_OF_MAIN()
